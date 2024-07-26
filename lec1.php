e6ea1a87b1f31f76eb6f39c0690e665239fca2b8
Shravan103 <shravanadarkar2003@gmail.com>
2024-07-26 11:51:55 +0530

    recet commit

--- lec1.php
+++ lec1.php
@@ -1,6 +1,7 @@
 <!-- lecture 2 -->
 <!-- index get automatically detected -->
 <!-- changing name index to lec1.php -->
+<!-- The files are added to the GitHub -->
 <?php
     echo "Hello world";
     echo "<br>";     //adds newline
